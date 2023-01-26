b7b6d3654a37cfc2288de0b07c4db6fb901fdb31
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-01-26 16:38:16 +0300

    w

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,16 @@
 package school.mjc.stage0.conditions.task4;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+            if (first >= second && first >= third){
+                System.out.println(first);
+        }   else if (second >= third && second >= first) {
+                System.out.println(second);
+            }   else {
+                    System.out.println(third);
+                }
+
     }
 }
